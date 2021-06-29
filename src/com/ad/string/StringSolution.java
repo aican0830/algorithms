e81719b711ad367556ecc6a5bbed8e5230bb1c65
deai <aican0830@163.com>
2021-06-29 22:09:09 +0800

    add merge-sorted-array,merge-sorted-array/

--- src/com/ad/string/StringSolution.java
+++ src/com/ad/string/StringSolution.java
@@ -6,6 +6,32 @@ import java.util.Map;
 public class StringSolution {
 
     /**
+     * 125. 验证回文串
+     * https://leetcode-cn.com/problems/valid-palindrome/
+     *
+     * @param s
+     * @return
+     */
+    public boolean isPalindrome(String s) {
+        int len = s.length();
+        StringBuffer sgood = new StringBuffer(); //保存字符串
+
+        for (int i = 0; i < len; i++) {
+
+            char ch = s.charAt(i); //取得字符
+            if (Character.isLetterOrDigit(ch)) { //过滤特殊字符
+                sgood.append(Character.toLowerCase(ch));
+            }
+        }
+
+        //字符串反转
+        StringBuffer sgoodRev = (new StringBuffer(sgood)).reverse();
+
+        //反转后如果相等则是回文字符串
+        return sgood.toString().equals(sgoodRev.toString());
+    }
+
+    /**
      * 344. 反转字符串
      * https://leetcode-cn.com/problems/reverse-string/
      *

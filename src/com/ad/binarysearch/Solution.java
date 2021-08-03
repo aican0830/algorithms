61709f322a0be833ade797d6751177cc6cef4104
aidong <aican0830@163.com>
2021-08-03 23:31:48 +0800

    add 34. Find First and Last Position of Element in Sorted Array

--- src/com/ad/binarysearch/Solution.java
+++ src/com/ad/binarysearch/Solution.java
@@ -40,4 +40,39 @@ public class Solution
         }
         return -1;
     }
+
+    /**
+     * 34. 在排序数组中查找元素的第一个和最后一个位置
+     * https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] searchRange(int[] nums, int target) {
+
+        int leftIdx = bs(nums, target, true);
+        int rightIdx = bs(nums, target, false) - 1;
+        if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] == target && nums[rightIdx] == target) {
+            return new int[]{leftIdx, rightIdx};
+        }
+        return new int[]{-1, -1};
+
+    }
+
+    public int bs(int[] nums, int target, boolean lower) {
+        int left = 0, right = nums.length - 1, ans = nums.length;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return ans;
+    }
 }
